cac0b8a4788e63d61fc3b311c0461573e738156d
Nicolas Guarini <nikiguarincia@gmail.com>
2022-10-16 20:30:29 +0200

    Dependencies and file structure setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function EncryptiaApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default EncryptiaApp
