38e89b37a88be909472ffcacc342016f9b207b41
Nicolas Guarini <nikiguarincia@gmail.com>
2022-10-26 00:07:40 +0200

    Implement AES encryption

--- pages/api/aes.js
+++ pages/api/aes.js
@@ -1,56 +1,50 @@
 //api/aes
-import * as Constants from '../../utils/constants'
 
 export default function handler(req, res) {
     if(req.method == 'GET'){
         const CryptoJS = require('crypto-js')
-        const query = req.query
-
-        const {plaintext, key, mode, ciphertext, iv} = query
+        const {plaintext, key, iv, ciphertext} = req.query
         
-        if(!Constants.modesMap.has(mode)){
-            res.status(400).send({
-                message: `Specified mode '${mode}' not supported by AES`
-            })
-        }else{
-            try{
-                if(!ciphertext){
-                    const encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(plaintext), CryptoJS.enc.Utf8.parse(key), {
-                        mode: Constants.modesMap.get(mode),
-                        padding: CryptoJS.pad.Pkcs7,
-                        iv: iv == null ? CryptoJS.enc.Utf8.parse('') : CryptoJS.enc.Utf8.parse(iv)
-                    })
-    
-                    res.status(200).json({
-                        plaintext: plaintext,
-                        key: key,
-                        ciphertext: encrypted.ciphertext.toString(),
-                        mode: mode,
-                        padding: 'Pkcs7'
-                    })
-                }else{
-                    const decrypted = CryptoJS.AES.decrypt({
-                        ciphertext: CryptoJS.enc.Hex.parse(ciphertext)
-                    }, CryptoJS.enc.Utf8.parse(key), {
-                        mode: Constants.modesMap.get(mode),
-                        padding: CryptoJS.pad.Pkcs7,
-                        iv: iv == null ? CryptoJS.enc.Utf8.parse('') : CryptoJS.enc.Utf8.parse(iv)
-                    })
+        const ivHex = CryptoJS.enc.Utf8.parse(iv ?? '0000')
+        const keyHex = CryptoJS.enc.Utf8.parse(key)
+        
+        const options = {
+            iv: ivHex, 
+            mode: CryptoJS.mode.CTR,
+            padding: CryptoJS.pad.Pkcs7
+        }
+        
+        try{
+            if(!ciphertext){
+                const encrypted = CryptoJS.AES.encrypt(plaintext, keyHex, options)
 
-                    res.status(200).json({
-                        plaintext: decrypted.toString(CryptoJS.enc.Utf8),
-                        key: key,
-                        ciphertext: ciphertext,
-                        mode: mode,
-                        padding: 'Pkcs7'
-                    })
-                } 
-            }catch(error){
-                console.log(error)
-                res.status(500).send({
-                    message: error.toString()
+                res.status(200).json({
+                    plaintext: plaintext,
+                    ciphertext: encrypted.toString(),
+                    key: key,
+                    iv: ivHex.toString(),
+                    mode: 'CTR'
                 })
-            }
-        }   
+            }else{
+                const encrypted = CryptoJS.enc.Base64.parse(ciphertext)
+
+                const decrypted  = CryptoJS.AES.decrypt({
+                    ciphertext: encrypted
+                }, keyHex, options)
+
+                res.status(200).json({
+                    plaintext: decrypted.toString(CryptoJS.enc.Utf8),
+                    ciphertext: ciphertext,
+                    key: key,
+                    iv: ivHex.toString(),
+                    mode: 'CTR'
+                })
+            } 
+        }catch(error){
+            console.log(error)
+            res.status(500).send({
+                message: error.toString()
+            })
+        }
     }
 }
