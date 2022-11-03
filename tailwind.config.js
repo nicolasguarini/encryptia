58d171fbc40b396e1cc4bd8ba662b5d7cb13d5cd
Nicolas Guarini <nikiguarincia@gmail.com>
2022-11-03 14:03:55 +0100

    Override slate-900 color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          900: '#0B1120'
+        }
+      }
+    },
   },
   plugins: [],
 }
