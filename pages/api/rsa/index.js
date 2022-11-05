becd887b1456dcf7aa404ccdc5bf316d6d3c92e5
Nicolas Guarini <nikiguarincia@gmail.com>
2022-11-05 15:23:54 +0100

    Implement RSA API

--- pages/api/rsa/index.js
+++ pages/api/rsa/index.js
@@ -1,3 +1,70 @@
+import rsa from "node-rsa"
+
 export default function handler(req, res) {
-    res.status(200).json({'ciau': 'ciau'})
+    if(req.method == 'GET'){
+        try{
+            let {plaintext, ciphertext, publicKey, privateKey} = req.query
+
+            if(plaintext) plaintext = decodeURI(plaintext).replace(/%2b/g, '+')
+            if(ciphertext) ciphertext = decodeURI(ciphertext).replace(/%2b/g, '+')
+
+            if(!plaintext && !ciphertext){
+                res.status(400).send({
+                    message: 'You must specify either plaintext or ciphertext'
+                })
+            }else if(publicKey && privateKey){
+                res.status(400).send({
+                    message: 'You must specify either private or public key'
+                })
+            }else if(publicKey){ 
+                publicKey = new rsa().importKey(
+                    decodeURI(publicKey).replace(/%2b/g, '+')
+                )
+
+                if(plaintext){ //We have to public encrypt
+                    res.status(200).json({
+                        plaintext: plaintext,
+                        ciphertext: publicKey.encrypt(plaintext, 'base64'),
+                        publicKey: publicKey.exportKey('public')
+                    })
+                }else if(ciphertext){ //We have to public decrypt
+                    res.status(200).json({
+                        plaintext: publicKey.decryptPublic(ciphertext, 'utf8'),
+                        ciphertext: ciphertext,
+                        publicKey: publicKey.exportKey('public')
+                    })
+                }
+            }else if(privateKey){
+                privateKey = new rsa().importKey(
+                    decodeURI(privateKey).replace(/%2b/g, '+')
+                )
+
+                if(plaintext){ //We have to private encrypt
+                    res.status(200).json({
+                        plaintext: plaintext,
+                        ciphertext: privateKey.encryptPrivate(plaintext, 'base64'),
+                        privateKey: privateKey.exportKey('private')
+                    })
+                }else if(ciphertext){ //We have to private decrypt
+                    res.status(200).json({
+                        plaintext: privateKey.decrypt(ciphertext, 'utf8'),
+                        ciphertext: ciphertext,
+                        privateKey: privateKey.exportKey('private')
+                    })
+                }
+            }else{
+                res.status(400).send({
+                    message: 'No key specified'
+                })
+            }
+        }catch(error){
+            res.status(500).send({
+                message: error.toString()
+            })
+        }
+    }else if(req.method == 'POST'){
+        res.send(405).json({
+            message: "Method POST Not Allowed"
+        })
+    }
 }
